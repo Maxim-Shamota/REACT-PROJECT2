4c142a7e0891c358c3ae05e2c44b9851124055ed
Maxim <xbultazar@gmail.com>
2021-03-18 20:50:13 +0300

    finish

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,8 +3,9 @@ import React from "react";
 import PostListItem from "../post-list-item";
 import "./post-list.css";
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
+    // eslint-disable-next-line array-callback-return
     const elements = posts.map((item) => {
         if (typeof item === 'object' && isEmpty(item)) {
             const {id, ...itemProps} = item;
@@ -12,7 +13,9 @@ const PostList = ({posts, onDelete}) => {
             <li key={id} className='list-group-item'>
                 <PostListItem 
                     {...itemProps} 
-                    onDelete={() => onDelete(id)} />
+                    onDelete={() => onDelete(id)} 
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}/>
             </li>  
             )
         }
